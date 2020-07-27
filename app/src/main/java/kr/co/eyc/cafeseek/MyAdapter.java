744840419c918189d9a95a5631ca0754b8b45b2b
yechan <glqgkq384@naver.com>
2020-07-27 09:04:01 +0900

    fix4

--- app/src/main/java/kr/co/eyc/cafeseek/MyAdapter.java
+++ app/src/main/java/kr/co/eyc/cafeseek/MyAdapter.java
@@ -46,8 +46,7 @@ public class MyAdapter extends RecyclerView.Adapter {
             @Override
             public void onClick(View v) {
 
-                Intent intent= new Intent(v.getContext(), CafeInfoActivity.class);
-                intent.putExtra("title", Item2.title);
+
             }
         });
     }
